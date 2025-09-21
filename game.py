dc7fce27984d8c671cb7991fb9600715333e5d95
Daniel Darabos <darabos.daniel@gmail.com>
2025-09-21 17:44:28 +0200

    Isometric city.

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import math
+import random
 import pyxel
 from dataclasses import dataclass
 
@@ -16,7 +17,23 @@ class Player:
     height: int = 16
 
 
+@dataclass
+class Block:
+    x: float
+    y: float
+    z: float
+    width: int = 16
+    height: int = 4
+
+
 player = Player(100, 100)
+blocks = []
+
+for row in range(10):
+    for col in range(10):
+        height = random.randint(1, 4) ** 2
+        for h in range(height):
+            blocks.append(Block(-50 + col * 18, 150 + row * 20, h * 4))
 
 
 def update():
@@ -55,6 +72,9 @@ def update():
 
 def draw():
     pyxel.cls(1)
+    for block in sorted(blocks, key=lambda b: (b.y + b.x, b.z)):
+        screen_y = block.y // 2 - block.z
+        pyxel.blt(block.x + block.y // 2, screen_y, 0, 16, 0, 16, 16, colkey=0)
     pyxel.blt(player.x, player.y, 0, 0, 0, 16, 16, colkey=0)
 
 
